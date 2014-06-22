5fc2099a89a04976c076d325b86a4446a6c50b33
Hycis <hyciswu@gmail.com>
2014-06-22 00:57:49 -0700

    noisyrelu ready

--- NoisyReLU.lua
+++ NoisyReLU.lua
@@ -1,24 +1,42 @@
 local NoisyReLU, parent = torch.class('nn.NoisyReLU','nn.Module')
 
-function NoisyReLU:__init(sparsityFactor, threshold_lr, alpha, std)
+function NoisyReLU:__init(sparsityFactor, threshold_lr, alpha_range, std)
+
+   -- Params
+   -- sparsityFactor: the micro sparsity of signals through each neuron
+   -- threshold_lr: the learning rate of adjusting the threshold to the desired threshold
+   -- alpha_range: {start_weight, num_batches, final_weight} for setting the weight on the
+   --              contemporary sparsity when calculating the mean sparsity over many batches. 
+   --              For the start, it will place more weight on the contemporary, but as more
+   --              epoch goes through, the weight on contemporary batch should decrease, so
+   --              that mean_sparsity will be more stable. 
+   
+   
    parent.__init(self)
    
-   assert(0 <= sparsityFactor and sparsityFactor <= 1, 'sparsityFactor not within range [0, 1]')
-   assert(0 < alpha and alpha < 1, 'alpha not within range (0, 1)') 
    self.sparsityFactor = sparsityFactor or 0.1
    self.threshold_lr = threshold_lr or 0.1
    
    -- std for the noise, default is no noise
-   self.std = std or 1
+   self.std = std or 0.1
    
    -- larger alpha means putting more weights on contemporary value 
    -- when calculating the moving average mean
-   self.alpha = alpha or 0.01
+   self.alpha_range = alpha_range or {0.2, 1000, 0.02}
+   self.alpha = alpha_range[1]
+   self.decrement = (alpha_range[1] - alpha_range[3]) / alpha_range[2]
+   
    self.first_batch = true 
+   
+   assert(0 <= self.sparsityFactor and self.sparsityFactor <= 1, 
+         'sparsityFactor not within range [0, 1]')
+   assert(0 < self.alpha and self.alpha < 1, 
+         'alpha not within range (0, 1)')
+   assert(self.std >= 0, 'std has be >= 0')
    
    self.threshold = torch.Tensor()
    self.mean_sparsity = torch.Tensor()
-   self.val = 0
+   self.activated = torch.Tensor()
 end
 
 function NoisyReLU:updateOutput(input)
@@ -31,18 +49,20 @@ function NoisyReLU:updateOutput(input)
    
    if self.first_batch then
       self.threshold = torch.zeros(input:size(2))
+      self.activated:resizeAs(input)
    end
 
    input = input + noise
    
-   self.output:resizeAs(input)
+   -- find if a neuron is active
    for i=1,input:size(1) do 
-      self.output[i] = input[i]:gt(self.threshold)
-      self.output[i]:cmul(input[i])
+      self.activated[i] = torch.gt(input[i], self.threshold)
    end
+
+   self.output = torch.cmul(self.activated, input)
    
-   -- find the training sparsity of a batch output
-   sparsity = torch.gt(self.output, 0):sum(2):type('torch.FloatTensor'):div(self.output:size(2))
+   -- find the activeness of a neuron in each batch
+   sparsity = self.activated:sum(1):type('torch.FloatTensor'):div(self.output:size(1))
    sparsity:resize(self.output:size(2))
    
    -- recalculate mean sparsity, using exponential moving average   
@@ -50,17 +70,29 @@ function NoisyReLU:updateOutput(input)
       self.mean_sparsity = sparsity
       self.first_batch = false
    else
-      self.mean_sparsity = self.alpha * sparsity + (1 - self.alpha) * self.mean_sparsity
+      
+      if self.alpha < self.alpha_range[3] then
+         self.alpha = self.alpha_range[3]
+      
+      else
+         self.alpha = self.alpha - self.decrement
+      end
+   
+      self.mean_sparsity = (sparsity * self.alpha
+                            + self.mean_sparsity
+                            * (1 - self.alpha))
    end
-   
-   -- update threshold, raise the threshold if the training activeness is larger than the desired activeness
-   self.threshold = self.threshold + self.threshold_lr * (self.mean_sparsity - self.sparsityFactor)
-   
+
    return self.output
 end
 
 function NoisyReLU:updateGradInput(input, gradOutput)
-   input.nn.NoisyReLU_updateGradInput(self, input, gradOutput)
+   self.gradInput = torch.cmul(gradOutput, self.activated)
+   
+   -- update threshold, raise the threshold if the training activeness is larger than the desired activeness
+   self.threshold = ((self.mean_sparsity - self.sparsityFactor)
+                     * self.threshold_lr + self.threshold)
+
    return self.gradInput
 end
 
