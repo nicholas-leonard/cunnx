0c6f7b4b223826a0fd3564566dd8ce4ca27fa6b3
nicholas-leonard <nick@nikopia.org>
2014-06-22 14:38:00 -0400

    ready to debug WindowGate.lua

--- WindowGate.cu
+++ WindowGate.cu
@@ -1,9 +1,9 @@
 #define WINDOWGATE_THREADS 128
 
 __global__ void cunnx_WindowGate_updateOutput_kernel(
-  float *output, float *inputIndiceCuda, float *outputIndiceCuda,
-  const float* input, int inputSize, int outputSize, int inputWindowSize, 
-  int outputWindowSize, int windowStride, int softmax, float a, float b)
+  float *output, float *centroids, float *outputIndiceCuda,
+  const float* input, int inputSize, int outputSize, 
+  int outputWindowSize, int windowStride, float a, float b)
 {
   __shared__ float buffer[WINDOWGATE_THREADS];
   unsigned int tx = threadIdx.x;
@@ -14,7 +14,7 @@ __global__ void cunnx_WindowGate_updateOutput_kernel(
   // get coordinate of centoid
   buffer[tx] = 0;
   for (unsigned int i=tx; i<inputSize; i+=blockDim.x)
-    buffer[tx] += (float)i*input_k[i];
+    buffer[tx] += (float)(i+1)*input_k[i];
   
   // add (reduce)
   for (unsigned int stride = WINDOWGATE_THREADS >> 1; stride > 0; stride >>= 1)
@@ -28,37 +28,34 @@ __global__ void cunnx_WindowGate_updateOutput_kernel(
   {
     float centroid = buffer[0];
     
-    if (!softmax) 
-      centroid /=inputSize;
-    
-    // inputIdx is left corner of window
-    int inputIdx = centroid - (float)inputWindowSize*0.5;
-    
     // make centroid a number between 0 and 1
-    centroid /=inputSize;
+    centroid /= inputSize;
     
-    int outputIdx = centroid/(float)(inputSize*outputSize) - (float)outputWindowSize*0.5;
+    // align centroid to output
+    centroid *= (float)outputSize;
+    
+    float outputIdx = centroid - (float)outputWindowSize*0.5;
 
     // clip indices
-    inputIdx = fminf(inputIdx, inputSize-inputWindowSize);
-    inputIdx = fmaxf(inputIdx, 1);
     outputIdx = fminf(outputIdx, outputSize-outputWindowSize);
-    outputIdx = fmaxf(outputidx, 1);
+    outputIdx = fmaxf(outputIdx, 1);
     
-    inputIndiceCuda[k] = inputIdx;
-    outputIndiceCuda[k] = outputIdx;
+    // align centroid to outputWindow
+    centroid -= outputIdx;
+    
+    outputIndiceCuda[k] = (int)outputIdx;
+    centroids[k] = centroid;
     buffer[0] = centroid;
   }
   
   __syncthreads();
   
-  float centroid = buffer[0] - 1;
+  float centroid = buffer[0];
    
   // gaussian blur 
-  return range:add(-mean):pow(2):mul(b):exp():mul(a)
   for (int i=tx; i<outputWindowSize; i+=blockDim.x)
   {
-    float x = (float)i-centroid;
+    float x = (float)(i+1)-centroid;
     output_k[i] = a*expf(x*x*b);
   }
 }
@@ -69,57 +66,57 @@ static int cunnx_WindowGate_updateOutput(lua_State *L)
   
   int inputSize = luaT_getfieldcheckint(L, 1, "inputSize");
   int outputSize = luaT_getfieldcheckint(L, 1, "outputSize");
-  int inputWindowSize = luaT_getfieldcheckint(L, 1, "inputWindowSize");
   int outputWindowSize = luaT_getfieldcheckint(L, 1, "outputWindowSize");
   int windowStride = luaT_getfieldcheckint(L, 1, "windowStride");
   int batchSize = luaT_getfieldcheckint(L, 1, "batchSize");
-  int softmax = luaT_getfieldcheckboolean(L, 1, "softmax");
+  float a = (float)luaT_getfieldchecknumber(L, 1, "a");
+  float b = (float)luaT_getfieldchecknumber(L, 1, "b");
   
-  THCudaTensor *inputIndiceCuda = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "inputIndiceCuda", "torch.CudaTensor");
-  THLongTensor *inputIndice = (THLongTensor*)luaT_getfieldcheckudata(L, 1, "inputIndice", "torch.LongTensor");
   THCudaTensor *outputIndiceCuda = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "outputIndiceCuda", "torch.CudaTensor");
   THLongTensor *outputIndice = (THLongTensor*)luaT_getfieldcheckudata(L, 1, "outputIndice", "torch.LongTensor");
-  THCudaTensor *output = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "output", "torch.CudaTensor");
+  THCudaTensor *centroid = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "centroid", "torch.CudaTensor");
+  THCudaTensor *output = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "_output", "torch.CudaTensor");
   
   luaL_argcheck(L, input->nDimension == 2, 2, "2D(batch mode) tensor expected");
   luaL_argcheck(L, input->size[1] == inputSize, 2, "invalid input size"); 
   
   THCudaTensor_resize1d(output, batchSize, outputWindowSize);
-  THCudaTensor_resize1d(inputIndiceCuda, batchSize);
   THCudaTensor_resize1d(outputIndiceCuda, batchSize);
+  THLongTensor_resize1d(outputIndice, batchSize);
     
   /* call cudakernel */
   dim3 blocks(batchSize); // each cuda-block is an example
   dim3 threads(WINDOWGATE_THREADS);
   cunnx_WindowGate_updateOutput_kernel<<<blocks,threads>>>(
-    THCudaTensor_data(output), THCudaTensor_data(inputIndiceCuda),
+    THCudaTensor_data(output), THCudaTensor_data(centroid), 
     THCudaTensor_data(outputIndiceCuda),
     (const float*)THCudaTensor_data(input), inputSize, outputSize,
-    inputWindowSize, outputWindowSize, windowStride, softmax
+    outputWindowSize, windowStride
   );
   
-  THLongTensor_copyCuda(inputIndice, inputIndiceCuda);
   THLongTensor_copyCuda(outputIndice, outputIndiceCuda);
 
   return 0;
 }
 
-
 __global__ void cunnx_WindowGate_updateGradInput_kernel(
-  float *output, float *inputIndiceCuda, float *outputIndiceCuda,
-  const float* input, int inputSize, int outputSize, int inputWindowSize, 
-  int outputWindowSize, int windowStride, int softmax, float a, float b)
+  float *gradInput, float *error, const float *centroids,
+  const float *input, const float *outputIndiceCuda,
+  const float* output, const float* gradOutput, 
+  int inputSize, int outputSize, int outputWindowSize,, 
+  float c, float d, float e, float lr)
 {
-  __shared__ float buffer[WINDOWGATE_THREADS];
+  __shared__ float buffer[WINDOWGATE_THREADS+1];
   unsigned int tx = threadIdx.x;
   unsigned int k = blockIdx.x;
+  const float *gradOutput_k = gradOutput + outputWindowSize*k;
+  const float *output_k = output + outputWindowSize*k;
   const float *input_k = input + inputSize*k;
-  float *output_k = output + outputWindowSize*k;
   
-  // get coordinate of centoid
+  // get gradient of centroid
   buffer[tx] = 0;
-  for (unsigned int i=tx; i<inputSize; i+=blockDim.x)
-    buffer[tx] += (float)i*input_k[i];
+  for (unsigned int i=tx; i<outputWindowSize; i+=blockDim.x)
+    buffer[tx] += (float)gradOutput_k[i]*output_k[i];
   
   // add (reduce)
   for (unsigned int stride = WINDOWGATE_THREADS >> 1; stride > 0; stride >>= 1)
@@ -131,83 +128,84 @@ __global__ void cunnx_WindowGate_updateGradInput_kernel(
   
   if (tx == 0)
   {
-    float centroid = buffer[0];
-    
-    if (!softmax) 
-      centroid /=inputSize;
-    
-    // inputIdx is left corner of window
-    int inputIdx = centroid - (float)inputWindowSize*0.5;
-    
-    // make centroid a number between 0 and 1
-    centroid /=inputSize;
-    
-    int outputIdx = centroid/(float)(inputSize*outputSize) - (float)outputWindowSize*0.5;
-
-    // clip indices
-    inputIdx = fminf(inputIdx, inputSize-inputWindowSize);
-    inputIdx = fmaxf(inputIdx, 1);
-    outputIdx = fminf(outputIdx, outputSize-outputWindowSize);
-    outputIdx = fmaxf(outputidx, 1);
-    
-    inputIndiceCuda[k] = inputIdx;
-    outputIndiceCuda[k] = outputIdx;
-    buffer[0] = centroid;
+    int outputIdx = outputIndice[k];
+    float centroid = centroids[k];
+    float gradCentroid = buffer[0]*c;
+    centroid -= (lr*gradCentroid);
+    centroid += outputIdx;
+    centroid /= outputSize;
+    buffer[WINDOWGATE_THREADS] = centroid*inputSize;
   }
   
   __syncthreads();
-  
-  float centroid = buffer[0] - 1;
+  float targetCentroid = buffer[WINDOWGATE_THREADS];
    
-  // gaussian blur 
-  return range:add(-mean):pow(2):mul(b):exp():mul(a)
-  for (int i=tx; i<outputWindowSize; i+=blockDim.x)
+  buffer[tx] = 0;
+  // target is a gaussian blur 
+  for (int i=tx; i<inputSize; i+=blockDim.x)
   {
-    float x = (float)i-centroid;
-    output_k[i] = a*expf(x*x*b);
+    float target = (float)(i+1)-targetCentroid;
+    target = d*expf(target*target*e);
+    float input = input_k[i];
+    // dot product of logProbInput and probTarget (NLL)
+    buffer[i] -= logf(input)*target;
+    // grad input w.r.t. NLL
+    gradInput_k[i] = -target/input;
   }
+  
+  // add (reduce)
+  for (unsigned int stride = WINDOWGATE_THREADS >> 1; stride > 0; stride >>= 1)
+  {
+    __syncthreads();
+    if (tx < stride)
+      buffer[tx] -= buffer[tx+stride];
+  }
+  
+  if (tx == 0)
+    error[k] = buffer[tx];
 }
 
   
 static int cunnx_WindowGate_updateGradInput(lua_State *L)
 { 
-  THCudaTensor *input = (THCudaTensor*)luaT_checkudata(L, 2, "torch.CudaTensor");  
+  THCudaTensor *input = (THCudaTensor*)luaT_checkudata(L, 2, "torch.CudaTensor");
+  THCudaTensor *gradOutput = (THCudaTensor*)luaT_checkudata(L, 3, "torch.CudaTensor"); 
   
   int inputSize = luaT_getfieldcheckint(L, 1, "inputSize");
   int outputSize = luaT_getfieldcheckint(L, 1, "outputSize");
-  int inputWindowSize = luaT_getfieldcheckint(L, 1, "inputWindowSize");
   int outputWindowSize = luaT_getfieldcheckint(L, 1, "outputWindowSize");
   int windowStride = luaT_getfieldcheckint(L, 1, "windowStride");
   int batchSize = luaT_getfieldcheckint(L, 1, "batchSize");
-  int softmax = luaT_getfieldcheckboolean(L, 1, "softmax");
+  float c = (float)luaT_getfieldchecknumber(L, 1, "c");
+  float d = (float)luaT_getfieldchecknumber(L, 1, "d");
+  float e = (float)luaT_getfieldchecknumber(L, 1, "e");
+  float lr = (float)luaT_getfieldchecknumber(L, 1, "lr");
   
-  THCudaTensor *inputIndiceCuda = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "inputIndiceCuda", "torch.CudaTensor");
-  THLongTensor *inputIndice = (THLongTensor*)luaT_getfieldcheckudata(L, 1, "inputIndice", "torch.LongTensor");
+  THCudaTensor *error = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "error", "torch.CudaTensor");
+  THCudaTensor *output = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "_output", "torch.CudaTensor");
   THCudaTensor *outputIndiceCuda = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "outputIndiceCuda", "torch.CudaTensor");
-  THLongTensor *outputIndice = (THLongTensor*)luaT_getfieldcheckudata(L, 1, "outputIndice", "torch.LongTensor");
-  THCudaTensor *output = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "output", "torch.CudaTensor");
+  THCudaTensor *gradInput = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "gradInput", "torch.CudaTensor");
   
   luaL_argcheck(L, input->nDimension == 2, 2, "2D(batch mode) tensor expected");
   luaL_argcheck(L, input->size[1] == inputSize, 2, "invalid input size"); 
   
-  THCudaTensor_resize1d(output, batchSize, outputWindowSize);
-  THCudaTensor_resize1d(inputIndiceCuda, batchSize);
-  THCudaTensor_resize1d(outputIndiceCuda, batchSize);
+  THCudaTensor_resize1d(gradInput, batchSize, inputSize);
+  THCudaTensor_resize1d(error, batchSize);
     
   /* call cudakernel */
   dim3 blocks(batchSize); // each cuda-block is an example
   dim3 threads(WINDOWGATE_THREADS);
-  cunnx_WindowGate_updateOutput_kernel<<<blocks,threads>>>(
-    THCudaTensor_data(output), THCudaTensor_data(inputIndiceCuda),
-    THCudaTensor_data(outputIndiceCuda),
-    (const float*)THCudaTensor_data(input), inputSize, outputSize,
-    inputWindowSize, outputWindowSize, windowStride, softmax
+  cunnx_WindowGate_updateGradInput_kernel<<<blocks,threads>>>(
+    THCudaTensor_data(gradInput), THCudaTensor_data(error), 
+    (const float*)THCudaTensor_data(centroid)
+    (const float*)THCudaTensor_data(input), 
+    (const float*)THCudaTensor_data(outputIndiceCuda),
+    (const float*)THCudaTensor_data(output), 
+    (const float*)THCudaTensor_data(gradOutput), 
+    inputSize, outputSize, outputWindowSize, c, d, e, lr
   );
   
-  THLongTensor_copyCuda(inputIndice, inputIndiceCuda);
-  THLongTensor_copyCuda(outputIndice, outputIndiceCuda);
-
-  return 0;
+  return 1;
 }
 
 
--- WindowGate.lua
+++ WindowGate.lua
@@ -13,99 +13,58 @@ local WindowGate, parent = torch.class('nn.WindowGate', 'nn.Module')
 -- TODO add gaussian jumps for robustness
 ------------------------------------------------------------------------
 
-function WindowGate:__init(outputWindowSize, outputSize, softmax, stdv)
+function WindowGate:__init(outputWindowSize, outputSize, inputStdv, outputStdv, lr)
    parent.__init(self)
    self.outputWindowSize = outputWindowSize
    self.outputSize = outputSize
-   self.softmax = softmax or true
-   self.stdv = stdv or outputWindowSize/2
-   self.a = 1/(self.stdv*math.sqrt(2*math.pi))
-   self.b = -1/(2*self.stdv*self.stdv)
+   self.inputStdv = inputStdv or 2
+   self.outputStdv = outputStdv or outputWindowSize/2
+   
+   self.a = 1/(self.outputStdv*math.sqrt(2*math.pi))
+   self.b = -1/(2*self.outputStdv*self.outputStdv)
+   self.c = 1/(self.outputStdv*self.outputStdv)
+   
+   self.lr = lr or 0.1
    
    self.outputIndice = torch.LongTensor()
-   self.inputIndice = torch.LongTensor()
-   self._outputIndice = torch.Tensor()
-   self._inputIndice = torch.Tensor()
    self._output = torch.Tensor()
-   self._outputWindow = torch.Tensor()
-   self.sum = torch.Tensor()
    self.centroid = torch.Tensor()
+   self.error = torch.Tensor()
    self.output = {self._output, self.outputIndice}
    self.batchSize = 0
+   
 end
 
 function WindowGate:updateOutput(input)
    assert(input:dim() == 2, "Only works with matrices")
    if self.batchSize ~= input:size(1) then
-      self.inputWindowSize = input:size(2)*self.outputWindowSize/self.outputSize
-      assert(self.inputWindowSize == math.ceil(self.inputWindowSize), "inputWindowSize should be an integer")
-      assert(self.inputWindowSize > 3, "windowSize is too small")
-      self.windowStride = self.outputWindowSize/self.inputWindowSize
-      assert(self.windowStride == math.ceil(self.windowStride), "windowStride should be an integer")
-      self.range = torch.repeatTensor(torch.range(1,input:size(2)):typeAs(input),input:size(1),1)
       self.batchSize = input:size(1)
-      self._output:resize(self.batchSize, self.outputWindowSize)
-      self.inputIndice:resize(self.batchSize)
       self.outputIndice:resize(self.batchSize)
+      self.inputStdv = inputStdv or input:size(2)/2
+      self.d = 1/(self.inputStdv*math.sqrt(2*math.pi))
+      self.e = -1/(2*self.inputStdv*self.inputStdv)
    end
-   if input.nn.WindowGate_updateOutput then
-      input.nn.WindowGate_updateOutput(input)
-      return self.output
-   end
-   self.sum:cmul(self.range, input)
-   -- get coordinate of centoid
-   if self.softmax then
-      self.centroid:sum(self.sum, 2)
-   else
-      self.centroid:mean(self.sum, 2)
-   end
-   -- make centroids a number between 0 and 1
-   self.centroid:div(input:size(2))
-   -- indices
-   self._inputIndice:mul(self.centroid, input:size(2)):add(-self.inputWindowSize*0.5)
-   self.inputIndice:copy(self._inputIndice)
-   self._outputIndice:mul(self.centroid, self.outputSize):add(-self.outputWindowSize*0.5)
-   self.outputIndice:copy(self._outputIndice)
-   for i=1,self.batchSize do
-      -- clip indices
-      self.inputIndice[i] = math.min(self.inputIndice[i], input:size(2)-self.inputWindowSize)
-      self.inputIndice[i] = math.max(self.inputIndice[i], 1)
-      self.outputIndice[i] = math.min(self.outputIndice[i], self.outputSize-self.outputWindowSize)
-      self.outputIndice[i] = math.max(self.outputIndice[i], 1)
-      -- expand window
-      local inputWindow = input[i]:narrow(1, self.inputIndice[i], self.inputWindowSize)
-      local outputWindow = torch.repeatTensor(inputWindow, self.windowStride, 1)
-      self._output[i]:copy(outputWindow:t():reshape(self.outputWindowSize))
-   end
+   input.nn.WindowGate_updateOutput(input)
    return self.output
 end
 
 function WindowGate:updateGradInput(input, gradOutputTable)   
    local gradOutput = gradOutputTable[1]
-   self.gradInput:resizeAs(input)
-   -- fill with default gradient for non-window inputs
-   self.gradInput:fill(self.gravity)
-   for i=1,self.batchSize do
-      -- reduce window
-      local gradInputWindow = self.gradInput[i]:narrow(1, self.inputIndice[i], self.inputWindowSize)
-      local gradOutputWindow = gradOutput[i]
-      local k = 1
-      for j=1,self.outputWindowSize,self.windowStride do
-         gradInputWindow[k] = gradOutputWindow:narrow(1, j, self.windowStride):sum()
-         k = k + 1
-      end
-   end
-   return self.gradInput
+   return input.nn.WindowGate_updateGradInput(input, gradOutput)
 end
 
 function WindowGate:type(type)
-   assert(type ~= 'torch.CudaType', "torch.CudaType not supported")
    self._output = self._output:type(type)
    self.gradInput = self.gradInput:type(type)
+   self.centroid = self.centroid:type(type)
+   self.error = self.error:type(type)
    self.output = {self._output, self.indice}
+   if type == 'torch.CudaTensor' then
+      self.outputIndiceCuda = torch.CudaTensor()
+   end
 end
 
-function blur(mean, stdv, size)
+local function blur(mean, stdv, size)
    local range = torch.range(1,size):float()
    local a = 1/(stdv*math.sqrt(2*math.pi))
    local b = -1/(2*stdv*stdv)
