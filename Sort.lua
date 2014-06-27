3c218dfc94c80e836d34e88c7ca9c20e7605c1b5
nicholas-leonard <nick@nikopia.org>
2014-06-27 12:53:49 -0400

    nn.Sort should also work with cuda

--- Sort.lua
+++ Sort.lua
@@ -14,16 +14,29 @@ function Sort:__init(dim, descending)
    
    self.indice = torch.LongTensor()
    self._output = torch.Tensor()
+   self._input = torch.Tensor()
    self.output = {self._output, self.indice}
+   self.cuda = false
 end
 
 function Sort:updateOutput(input)
    assert(input:dim() == 2, "Only works with matrices")
+   if self.cuda then
+      self._input:resize(input:size())
+      self._input:copy(input)
+      input = self._input
+   end
    self._output:sort(self.indice, input, self.dim, self.descending)
+   if self.cuda then
+      self._outputCuda:resize(output:size())
+      self._outputCuda:copy(self._output)
+      self._indiceCuda:resize(self.indice:size())
+      self._indiceCuda:copy(self.indice)
+   end
    return self.output
 end
 
-function Sort:updateGradInput(input, gradOutput)   
+function Sort:updateGradInput(input, gradOutput)
    local dim = self.notDim
    self.gradInput:resizeAs(input)
    for i=1,input:size(dim) do
@@ -33,10 +46,19 @@ function Sort:updateGradInput(input, gradOutput)
 end
 
 function Sort:type(type)
-   assert(type ~= 'torch.CudaType', "torch.CudaType not supported")
-   self._output = self._output:type(type)
    self.gradInput = self.gradInput:type(type)
-   self.output = {self._output, self.indice}
+   if (type ~= 'torch.CudaType') then
+      self._output = self._output:type(type)
+      self._input = self._input:type(type)
+      self.output = {self._output, self.indice}
+   else
+      self.cuda = true
+      self._output = self._output:float()
+      self._input = self._input:float()
+      self._outputCuda = torch.CudaTensor()
+      self._indiceCuda = torch.CudaTensor()
+      self.output = {self._outputCuda, self.indiceCuda}
+   end
 end
 
 
--- init.cu
+++ init.cu
@@ -19,7 +19,6 @@ printf( "Assert failed %d:%d at %s:%d\n", blockIdx.x, threadIdx.x,  __FILE__, __
 #include "WindowSparse.cu"
 #include "WindowGate.cu"
 #include "WindowGate2.cu"
-#include "NoisyReLU.cu"
 
 LUA_EXTERNC DLL_EXPORT int luaopen_libcunnx(lua_State *L);
 
@@ -32,7 +31,6 @@ int luaopen_libcunnx(lua_State *L)
   cunnx_WindowSparse_init(L);
   cunnx_WindowGate_init(L);
   cunnx_WindowGate2_init(L);
-  cunnx_NoisyReLU_init(L);
 
   return 1;
 }
