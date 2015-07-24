e94059e4ba3eaa438ed6c0068c2b4f6f3083fb16
Nicholas Leonard <nick@nikopia.org>
2015-07-24 16:02:05 -0400

    removed LinearNoBias

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ torch.include('cunnx', 'WindowSparse.lua')
 torch.include('cunnx', 'WindowGate.lua')
 torch.include('cunnx', 'WindowGate2.lua')
 torch.include('cunnx', 'WindowMixture.lua')
-torch.include('cunnx', 'LinearNoBias.lua')
+--torch.include('cunnx', 'LinearNoBias.lua')
 torch.include('cunnx', 'MultinomialStatistics.lua')
 
 torch.include('cunnx', 'BlockSparse.lua')
